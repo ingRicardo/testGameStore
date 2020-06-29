f525e80da3caa21c60665ce29472eb630e5d0836
r-macias <r.macias@samsung.com>
2020-06-29 13:15:23 -0700

    first commit

---
+++ testJAPconnect1/src/main/java/com/spring/app/TestJaPconnect1Application.java
@@ -0,0 +1,13 @@
+package com.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJaPconnect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJaPconnect1Application.class, args);
+	}
+
+}
